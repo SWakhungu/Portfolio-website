264f5080b1e168185f15c357f0a27bb0c80a0509
Steve Wakhungu <stwakhungu@gmail.com>
2022-09-09 15:41:36 +0300

    Add funtionality to scroll button for user

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ button.addEventListener('click', () => {
     button.classList.toggle('liked')
 })
 
-  
+let myButton = document.getElementById("myBtn");
+
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    {
+        myButton.style.display = "block";
+    }    else {
+        myButton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
