67b258f7902dbffbe2570bc9b243b7fcaa6a1aab
Steve Wakhungu <stwakhungu@gmail.com>
2022-09-12 23:37:04 +0300

    Add toggle function for phone number: hide/show

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ function topFunction(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
